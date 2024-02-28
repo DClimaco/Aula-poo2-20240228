47fd23f539a66c45b5bd7ffd718913a959b8d77f
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-02-28 08:48:22 -0300

    Add files via upload

---
+++ Aula-poo2-20240228/AgendaTelefonica.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_poo2_20240228
+{
+    internal class AgendaTelefonica
+    {
+        List<Contato> listaContatos = new List<Contato>();
+
+        public AgendaTelefonica(List<Contato> listaContatos)
+        {
+            listaContatos = new List<Contato>();
+            this.listaContatos = listaContatos;
+        }
+
+        public void Inserir (string nome, string telefone)
+        {
+            //Contato c = new Contato(nome, telefone);
+            listaContatos.Add(new Contato(nome, telefone));
+        }
+
+        public Contato Buscar (string nome)
+        {
+            Contato c;
+
+            foreach(Contato x in listaContatos)
+            {
+                if (x.nome.Contains(nome))
+                {
+                    return x;
+                }
+            }
+            return null;
+        }
+    }
+}
